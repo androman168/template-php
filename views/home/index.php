ae7b2e972f680ceec16fdd1ec39c9acfa11564e6
androman168 <66146597+androman168@users.noreply.github.com>
2022-11-10 20:03:43 -0600

    Update template

---
+++ views/home/index.php
@@ -0,0 +1,1 @@
+<h1>Template prueba</h1>
---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+<div class="mt-5 p-4 bg-dark text-white text-center">
+	<p>Footer &copy; <?= date('Y') ?></p>
+</div>
+
+</body>
+
+</html>
